eb3843d3fe86154d3a19d43cfadb2edea279b192
Vitor Gabriel Pinheiro Gomes <141376701+Vitorgpg@users.noreply.github.com>
2024-08-19 22:24:50 -0300

    pela metade

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+Elabore um programa que:
+1. Solicite o nome de um produto
+2. solicite a quantidade vendida
+3. solicite o valor do produto 
+4. apresente o valor total
+5. solicite a % de desconto 
+6. apressente o R$ total de desconto
+7. apresente o valor total da venda depois do desconto 
+*/
+
+var nome = prompt("Digite o nome de algum produto:");
+var qntd = prompt("Digite a quantos produtos tem:");
+var valor = prompt("Digite o valor do produto:");
+var valorTotal = qntd + valor;
+console.log(valorTotal);
+
